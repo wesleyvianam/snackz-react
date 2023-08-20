406db3709e0e52c070e593fc5b51d75d00f4c2bc
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-20 16:45:26 -0300

    Refactor main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App/Page/App.jsx'
 import axios from 'axios';
 
 const csrfTokenMeta = document.querySelector('meta[name="csrf-token"]');
